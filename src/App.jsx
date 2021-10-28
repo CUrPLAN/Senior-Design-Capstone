602139249b1a47244fa7476efe8196902ba961db
Ivy Truong <truong.ivy26@gmail.com>
2021-10-28 15:28:31 +0000

    Created flowchart  view branch, added comments, and created box elements to display classes.

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,11 @@ class FlowChartItem extends React.Component {
   }
 
   render() {
-    return (<div>Some flow chart element</div>);
+    return (
+      <div className='flow-box'>
+        {this.props.Name}
+      </div>
+    );
   }
 }
 
@@ -38,22 +42,24 @@ class App extends React.Component {
   }
 
   // load data from json, set state with information
-  componentDidMount() {
-    fetch('csreqs.json')
+  componentDidMount() { // runs when component loads
+    fetch('csreqs.json') // get file at csreqs.json asyncronously
       .then(response => response.text())
       .then(json => JSON.parse(json))
-      .then(data => this.setState(data))
-      .catch(e => console.error('Couldn\'t read json file. The error was:\n', e));
+      .then(data => this.setState(data)) // set state information
+      .catch(e => console.error('Couldn\'t read json file. The error was:\n', e)); // print any errors
   }
 
+  // function for handling a click on one of the top navbar links
   menuClick(i) {
-    if (i === 0) {
+    if (i === 0) { // if the first button is clicked
       this.setState({Display: 'Flow'});
-    } else if (i === 1) {
+    } else if (i === 1) { // if the second button is clicked
       this.setState({Display: 'Edit'});
     }
   }
 
+  // function that handles creating the edit view and list view components, with the json info that needs to be displayed
   displayEditView() {
     return (<div>
       <ListViewItem></ListViewItem>
@@ -62,18 +68,29 @@ class App extends React.Component {
 
   }
 
+  // function that handles the content from the json file that should be displayed, and labels the semesters accordingly
   displayFlowChart() {
+    // stores the classes, semester type, and year for each semester
     let semesters = [];
+    // i = number of years to graduate in
+    // j = number of semesters per year
     for (let i = 0; i < 4; i++) {
       for (let j = 0; j < 2; j++) {
+        // Col: column tag, imported from bootstrap-react
+        // key attribute is used as a unique identifier for an item in a list in feact
+        // p tag will display Spring if j = 2, otherwise Fall will be displayed; shorthanded if/else statement used
+        // i+1 is displayed as the year because the loop starts at 0
+        // FlowchartItem tag will contain the information about the class (what class you are taking that semester will be displayed)
         semesters.push(
           <Col key={'year' + i + 'sem' + j}>
             <p>{j ? 'Spring' : 'Fall'} {i+1}</p>
-            <FlowChartItem></FlowChartItem>
+            <FlowChartItem {...this.state.Classes[0]}></FlowChartItem>
           </Col>
         );
       }
     }
+    // the display flowchart function will return the html for entire flowchart
+    // since the html code is stored in a variable, the curly brackets are used to denote that the html code in the object should be inserted at this spot
     return (
       <Container>
         <Row>
@@ -83,17 +100,22 @@ class App extends React.Component {
     );
   }
 
+  // render function under App class is used to tell application to display content
   render() {
     console.log("Rendering:");
     console.log("Have state information of:");
     console.log(this.state);
 
-    let content;
+    let content; // variable to store the content to render
+    // set content to display based on which tab the user is currently in (the mode they currently see)
     if (this.state.Display === 'Edit') {
       content = this.displayEditView();
     } else {
       content = this.displayFlowChart();
     }
+    // this return function in the render function will display the content
+    // it creates the html code for the navbars and basic layout of the page
+    // the {content} segment indicates that the html code from the variable above should be inserted
     return (
       <div className="App">
         <Navbar variant='dark' bg='dark' sticky='top'>
