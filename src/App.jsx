68fc0dcda301357a487e15ad1786704903ebb1b8
palaciosherzog <72314025+palaciosherzog@users.noreply.github.com>
2022-02-25 02:20:17 +0000

    collapsing table, added science credit option

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import InputGroup from 'react-bootstrap/InputGroup';
@@ -11,8 +12,7 @@ import saveAs from 'file-saver';
 import Dropzone from 'react-dropzone';
 import Popover from 'react-bootstrap/Popover';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-
-
+import Table from 'react-bootstrap/Table';
 
 // adapted from https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects
 // call groupby(list to group, function which takes one argument which will be called on each element of the list to determine groups)
@@ -23,6 +23,7 @@ var groupBy = function(xs, func) {
   }, {});
 };
 
+// *******function that obtains the prerequisite information to display if it exists*******
 var getNotes = function(oldPrereqs) {
   let prereqs = oldPrereqs.slice();
   if (!(typeof prereqs == 'undefined') && (prereqs.length > 0)) {
@@ -105,6 +106,7 @@ class FlowChartItem extends React.Component {
   }
 }
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -113,11 +115,12 @@ class App extends React.Component {
       Classes: [],
       Class_Desc: [],
       Taken_Classes: [],
-      CurPreReqs: []
+      CurPreReqs: [],
+      expandedRows: []
     };
   }
 
-  // load data from json, set state with information
+  // ************load data from json, set state with information************
   componentDidMount() { // runs when component loads
     fetch('csreqs.json') // get file at csreqs.json asyncronously
       .then(response => response.text())
@@ -126,80 +129,69 @@ class App extends React.Component {
       .catch(e => console.error('Couldn\'t read json file. The error was:\n', e)); // print any errors
   }
 
-  // function for handling a click on one of the top navbar links
+  // ************function for handling a click on one of the top navbar links*********
   menuClick(i) {
     if (i === 0) { // if the first button is clicked
       this.setState({ Display: 'Flow' });
-    } else if (i === 1) { // if the second button is clicked
+    } else if (i === 1 & !this.state.Display.startsWith('Edit')) { // if the second button is clicked
       this.setState({ Display: 'Edit' });
+    } else if (i === 2) { //if first submenu of second button is clicked
+      this.setState({ Display: 'EditGenEd' });
+    } else if (i == 3) { //if second submenu of second button is clicked
+      this.setState({ Display: 'EditCS' });
     }
   }
 
-  addClassToCategory(classId, classCredits, category) {
-    this.state.Categories[category].Credits -= Number.parseInt(classCredits.slice(0,1));
+  addClassToFlowchart(classId, category) {
+    //to update a state, need to copy contents (using slice) into a new variable, modify the state in that new variable, then set the state (needed because of react's rendering requirements)
+    let newClasses = this.state.Classes.slice();
+    // for science credit to update properly, add an additional class to the flowchart if biology or chemistry path was taken
+    if (classId.startsWith('BIOL') || classId.startsWith('CHEM')) {
+      newClasses = [...this.state.Classes, {
+        "Name": "Science",
+        "Credits": "1-2 hours",
+        "Semester": "Spring-2",
+        "Color": "SCIENCE"
+      }];
+    }
     if ('FC_Name' in this.state.Categories[category]) {
-      let index = this.state.Classes.findIndex(c => c.Name === this.state.Categories[category].FC_Name);
-      let newClasses = this.state.Classes.slice();
-      newClasses[index].Type = newClasses[index].Name;
-      newClasses[index].Name = classId;
-      this.setState({ Classes: newClasses });
+      for (let fc_name of this.state.Categories[category].FC_Name) {
+        // tries to find a flowchart box that thic class can fill
+        let index = this.state.Classes.findIndex(c => c.Name === fc_name);
+        if (index >= 0) {
+          // creates a duplicate of the classes list & modifies info to have this class instead
+          newClasses[index].Type = newClasses[index].Name; // adds element to remember the category
+          newClasses[index].Name = classId;
+          break;
+        }
+      }
     }
+    this.setState({ Classes: newClasses });
   }
-
-  removeClassFromCategory(classId, classCredits, category) {
-    this.state.Categories[category].Credits += Number.parseInt(classCredits.slice(0,1));
+  
+  // ********remove a class when you unclick the toggle button***********
+  removeClassFromFlowchart(classId) {
+    // finds the class and replaces the information with old category again (when you unclick the class)
     let index = this.state.Classes.findIndex(cl => cl.Name === classId);
     if ('Type' in this.state.Classes[index]) {
       let newClasses = this.state.Classes.slice();
-      newClasses[index].Name = newClasses[index].Type;
+      newClasses[index].Name = newClasses[index].Type; // .Type is the old category
       this.setState({ Classes: newClasses });
     }
   }
 
-  // function for handling a click on a checkbox
+  // *************function for handling a click on a checkbox**********
   checkboxClick(classID) {
     // create a copy of the taken classes (for re-rendering purposes in React)
     if (this.state.Taken_Classes.indexOf(classID) > -1) { // if class is already in taken list
       // removes classid from taken classes list by creating a new list that does not include that classID
       this.setState({ Taken_Classes: this.state.Taken_Classes.filter(c => c !== classID) });
-      let cl = this.state.Class_Desc[classID];
-      if ('Classes' in this.state.Categories[cl.Fulfills] && this.state.Categories[cl.Fulfills].Classes.indexOf(classID) > -1) {
-        this.state.Categories[cl.Fulfills].Classes = this.state.Categories[cl.Fulfills].Classes.filter(c => c !== classID);
-        if ('Fallback_Cat' in this.state.Categories[cl.Fulfills]) {
-          this.removeClassFromCategory(classID, cl.Credits, this.state.Categories[cl.Fulfills].Fallback_Cat);
-        }
-      } else {
-        this.removeClassFromCategory(classID, cl.Credits, cl.Fulfills);
-        if ('Classes' in this.state.Categories[cl.Fulfills] && this.state.Categories[cl.Fulfills].Classes.length > 0) {
-          let newClassId = this.state.Categories[cl.Fulfills].Classes[0];
-          cl = this.state.Class_Desc[newClassId];
-          this.state.Categories[cl.Fulfills].Classes = this.state.Categories[cl.Fulfills].Classes.filter(c => c !== newClassId);
-          if ('Fallback_Cat' in this.state.Categories[cl.Fulfills]) {
-            this.removeClassFromCategory(newClassId, cl.Credits, this.state.Categories[cl.Fulfills].Fallback_Cat);
-          }
-          if (this.state.Categories[cl.Fulfills].Credits > 0) {
-            this.addClassToCategory(newClassId, cl.Credits, cl.Fulfills);
-          } else {
-            this.state.Categories[cl.Fulfills].Classes = ('Classes' in this.state.Categories[cl.Fulfills]) ? [...this.state.Categories[cl.Fulfills].Classes, newClassId] : [newClassId];
-            if ('Fallback_Cat' in this.state.Categories[cl.Fulfills]) {
-              this.addClassToCategory(newClassId, cl.Credits, this.state.Categories[cl.Fulfills].Fallback_Cat);
-            }
-          }
-        }
-      }
+      this.removeClassFromFlowchart(classID);
     } else {
       // sets state to be the list of previously selected taken classes, with the addition of the new classID
       // ... is the spread operator, it makes the elements into elements of the new array
       this.setState({Taken_Classes: [...this.state.Taken_Classes, classID]});
-      let cl = this.state.Class_Desc[classID];
-      if (this.state.Categories[cl.Fulfills].Credits > 0) {
-        this.addClassToCategory(classID, cl.Credits, cl.Fulfills);
-      } else {
-        this.state.Categories[cl.Fulfills].Classes = ('Classes' in this.state.Categories[cl.Fulfills]) ? [...this.state.Categories[cl.Fulfills].Classes, classID] : [classID];
-        if ('Fallback_Cat' in this.state.Categories[cl.Fulfills]) {
-          this.addClassToCategory(classID, cl.Credits, this.state.Categories[cl.Fulfills].Fallback_Cat);
-        }
-      }
+      this.addClassToFlowchart(classID, this.state.Class_Desc[classID].Fulfills);
     }
   }
 
@@ -208,7 +200,6 @@ class App extends React.Component {
     let reader = new FileReader();
     // when reader reads a file
     reader.onload = (e) => {
-      console.log("reader results", reader.result);
       try {
         this.setState({ Taken_Classes: JSON.parse(e.target.result) }); // populate Taken_Classes with parsed upload file
       } catch (err) { // if error during parsing to json or setting state
@@ -221,8 +212,7 @@ class App extends React.Component {
   }
 
   // this function handles the functionality of the upload button itself so the user can choose a file to upload
-  onClickUpload() {
-    document.getElementById('uploadFileButton').click();
+  onClickUpload() { document.getElementById('uploadFileButton').click();
   }
 
   // when begin hovering over box, get prereqs and put in state
@@ -256,7 +246,7 @@ class App extends React.Component {
     this.setState({ CurPreReqs: [] });
   }
 
-  //make sure to open the webpage into a new tab to test save click functionality
+  //*******make sure to open the webpage into a new tab to test save click functionality************
   saveClick() {
     // adapted from answer to https://stackoverflow.com/questions/45941684/save-submitted-form-values-in-a-json-file-using-react
     const fileData = JSON.stringify(this.state.Taken_Classes);
@@ -264,13 +254,72 @@ class App extends React.Component {
     saveAs(blob, "CUrPLAN");
   }
 
-  // function that handles creating the edit view and list view components, with the json info that needs to be displayed
-  displayEditView() {
-    let headerList = groupBy(Object.entries(this.state.Class_Desc), x => x[1].Fulfills);
+  // *****handles expanding rows when a header is clicked (like Gen Ed: English or CS Breadth: Data Science), takes in the categories of classes as input*******
+  handleRowClick(rowId) {
+    const currentExpandedRows = this.state.expandedRows;
+    const isRowCurrentlyExpanded = currentExpandedRows.includes(rowId);
+    //determines which rows to show based on which rows are clicked
+    const newExpandedRows = isRowCurrentlyExpanded ? 
+			currentExpandedRows.filter(id => id !== rowId) : currentExpandedRows.concat(rowId);
+      this.setState({expandedRows : newExpandedRows});
+  }
+
+  // *** creates main header row for the section, and addtional rows IFF expanded
+  renderItem(section) {
+    let [header, classes] = section;
+    const clickCallback = () => this.handleRowClick(header);
+
+    // add the main section row to a list 
+    const itemRows = [
+      <tr onClick={clickCallback} key={"row-data-" + header}> 
+        <td className="heading-courses-td"><div className="arrow down"></div>{header}</td>
+        {/* get needed credits for section by looking in object */}
+        <td className="heading-credits-td">{this.state.Categories[header].Credits}</td>
+        <td className="heading-notes-td"></td>
+      </tr>
+    ];
+    
+    if(this.state.expandedRows.includes(header)) {
+      classes.forEach(([courseId, course]) => (
+        itemRows.push(<ListViewItem
+                    key={'course' + courseId}
+                    show={this.state.toggle}
+                    Id={courseId}
+                    {...course} // pass elements of a course as properties to the ListViewItem
+                    // () => is a way to bind functions when needing to pass functions to another component
+                    changeFunc={() => this.checkboxClick(courseId)} // pass function to component: https://reactjs.org/docs/faq-functions.html
+                    checked={this.state.Taken_Classes.indexOf(courseId) > -1} // variable used to keep boxes checked when switch between views
+                  ></ListViewItem>))
+      );
+    }
+    return itemRows;    
+  }
+
+  // ********function that handles creating the edit view and list view components, with the json info that needs to be displayed***********
+  displayEditView(displayChoice) {
+    // gets object with groups of classes by headers
+    let headerList = Object.entries(groupBy(Object.entries(this.state.Class_Desc), x => x[1].Fulfills));
+    // filters list by argument passed in
+    if (displayChoice === 'EditGenEd') {
+      headerList = headerList.filter(x => x[0].includes('Gen Ed'));
+    }
+    if (displayChoice == 'EditCS') {
+      headerList = headerList.filter(x => !x[0].includes('Gen Ed'));
+    }
+
+    // list for storing rows of table
+    let allItemRows = [];
+
+    // calls to get the rows for each section of classes, adds them to list
+    headerList.forEach(item => {
+        const perItemRows = this.renderItem(item);
+        allItemRows = allItemRows.concat(perItemRows);
+    });
+    
     return (
       <React.Fragment>
       <div className="credit-count">{this.calculateSemHours(this.state.Classes)}</div>
-      <table className='listview-table'>
+      <Table className='listview-table'>
         <thead>
           <tr>
             <th className='courses'>Courses</th>
@@ -280,39 +329,19 @@ class App extends React.Component {
         </thead>
         <tbody>
           <tr>
-            <td colSpan="3" className="alert-td">* Course prerequisites change regularly. 
+            <td colSpan={3} className="alert-td">* Course prerequisites change regularly. 
             Students are responsible for consulting advisors and the class schedule in the student portal for prerequisite information. *</td>
           </tr>
 
-          <tr>
+          {/*<tr>
             <td className="heading-courses-td">Required CU Denver Core Curriculum Coursework</td>
             <td className="heading-credits-td">24</td>
             <td><a href="https://catalog.ucdenver.edu/cu-denver/undergraduate/graduation-undergraduate-core-requirements/cu-denver-core-curriculum/">
               See CU Denver Core Curriculum here</a></td>
-          </tr>
-
-          { // convert list of headers and courses to appropriate table rows
-            Object.entries(headerList).map(([header, courses]) => (
-              <React.Fragment key={'section' + header}>
-                <tr>
-                  <td className="heading-courses-td">{header}</td>
-                  {/* get needed credits for section by looking in object */}
-                  <td className="heading-credits-td">{this.state.Categories[header].Credits}</td>
-                  <td className="heading-notes-td"></td>
-                </tr>
-                { /* pass all properties of a course to the list view item to get the html code */
-                  courses.map(([courseId, course]) => (<ListViewItem
-                    key={'course' + courseId}
-                    Id={courseId}
-                    {...course} // pass elements of a course as properties to the ListViewItem
-                    // () => is a way to bind functions when needing to pass functions to another component
-                    changeFunc={() => this.checkboxClick(courseId)} // pass function to component: https://reactjs.org/docs/faq-functions.html
-                    checked={this.state.Taken_Classes.indexOf(courseId) > -1} // variable used to keep boxes checked when switch between views
-                  ></ListViewItem>))}
-              </React.Fragment>
-            ))}
-        </tbody>
-      </table>
+          </tr>*/}
+          { allItemRows }
+          </tbody>
+          </Table>
       </React.Fragment>
     );
   }
@@ -389,15 +418,16 @@ class App extends React.Component {
 
     let content; // variable to store the content to render
     // set content to display based on which tab the user is currently in (the mode they currently see)
-    if (this.state.Display === 'Edit') {
-      content = this.displayEditView();
-    } else {
+    let display = this.state.Display;
+    if (display === 'Flow') {
       content = this.displayFlowChart();
-    }
-
+    } else {
+      content = this.displayEditView(display);
+    } 
     // this return function in the render function will display the content
     // it creates the html code for the navbars and basic layout of the page
     // the {content} segment indicates that the html code from the variable above should be inserted
+    // react bootstrap nav dropdown menu link: https://react-bootstrap.github.io/components/dropdowns/
     return (
       <div className="App">
         <Navbar variant='dark' bg='dark' sticky='top'>
@@ -407,10 +437,18 @@ class App extends React.Component {
               className={(this.state.Display === 'Flow') ? 'active' : 'inactive'}
               onClick={() => this.menuClick(0)}
             >Flowchart</Nav.Link>
-            <Nav.Link 
-              className={(this.state.Display === 'Edit') ? 'active' : 'inactive'}
+            <NavDropdown
+              className={((this.state.Display.startsWith('Edit')) ? 'active' : 'inactive')}
               onClick={() => this.menuClick(1)}
-            >Edit Classes</Nav.Link>
+              title="Edit Classes" id="basic-nav-dropdown"
+              menuVariant="dark"
+              align="end"
+            >
+              <NavDropdown.Item
+              onClick={() => this.menuClick(2)}>General Ed Classes</NavDropdown.Item>
+              <NavDropdown.Item
+              onClick={() => this.menuClick(3)}>Computer Science BS</NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Navbar>
         <div className = "flow-warn">
